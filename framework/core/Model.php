261e7bbc6cc99f44ae001dcd86ea37ea4c5ea262
JhumanJ <julien@nahum.net>
2018-01-23 16:29:15 +0000

    Start working on the abstract model

--- framework/core/Model.php
+++ framework/core/Model.php
@@ -7,7 +7,27 @@
 abstract class Model
 {
 
-    public $db;
+    /**
+     * Name of database table holding entity
+     */
+    public $table;
+
+    /**
+     * List of public fields (that me be passed to view, or mass updated)
+     */
+    public $public_fields = [];
+
+    /**
+     * List of private or hidden fields (user password for instance)
+     */
+    public $private_fields = [];
+
+    /**
+     * Value casting of each attributes (not all fields must be cast)
+     */
+    public $casts = [];
+
+    protected $db;
 
     public function __construct()
     {
