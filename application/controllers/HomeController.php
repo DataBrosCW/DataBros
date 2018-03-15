43bdd69f30c0d80b613ced766e1c65b09b0ac073
JhumanJ <julien@nahum.net>
2018-03-15 20:30:38 +0000

    fixed

--- application/controllers/HomeController.php
+++ application/controllers/HomeController.php
@@ -4,8 +4,6 @@ class HomeController extends Controller
 {
     public function index()
     {
-        $product_id = auth_user()->mostVisitedProduct();
-        if (!$product_id) return null;
         $this->render('home');
     }
 
--- public/views/home.blade.php
+++ public/views/home.blade.php
@@ -11,36 +11,40 @@
             <p>Your personalised ebay experience</p>
         </div>
 
-        <div class="col-12 col-sm-6 col-md-4">
-            <div class="card">
-                <div class="card-header">
-                    Number of followed categories
-                </div>
-                <div class="card-body">
+        @if (auth_check())
 
-                </div>
-            </div>
-        </div>
-        <div class="col-12 col-sm-6 col-md-4">
-            <div class="card">
-                <div class="card-header">
-                    Number of favourite products
-                </div>
-                <div class="card-body">
+            <div class="col-12 col-sm-6 col-md-4">
+                <div class="card">
+                    <div class="card-header">
+                        Number of followed categories
+                    </div>
+                    <div class="card-body">
 
+                    </div>
                 </div>
             </div>
-        </div>
-        @if (auth_user()->mostVisitedProduct())
-        <div class="col-12 col-sm-6 col-md-4">
-            <div class="card">
-                <div class="card-header">
-                    Your most visited item
-                </div>
-                <div class="card-body">
+            <div class="col-12 col-sm-6 col-md-4">
+                <div class="card">
+                    <div class="card-header">
+                        Number of favourite products
+                    </div>
+                    <div class="card-body">
+
+                    </div>
                 </div>
             </div>
-        </div>
+            @if (auth_user()->mostVisitedProduct())
+                <div class="col-12 col-sm-6 col-md-4">
+                    <div class="card">
+                        <div class="card-header">
+                            Your most visited item
+                        </div>
+                        <div class="card-body">
+                        </div>
+                    </div>
+                </div>
+            @endif
+
         @endif
     </div>
 
